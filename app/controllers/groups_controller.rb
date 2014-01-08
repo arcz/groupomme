4d8ea76d90b3ff432714ff78e04dab5931c0b221
Artur Cygan <arczicygan@gmail.com>
2014-01-08 17:23:05 +0100

    Devise, simple_form, groups setup

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,23 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to @group
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+end
